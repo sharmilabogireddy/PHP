bdc745f4146fff56d958fe8fc8cb860dfabce9fc
sharmilabogireddy <42264620+sharmilabogireddy@users.noreply.github.com>
2019-03-06 13:21:54 +1300

    Add files via upload

--- db.php
+++ db.php
@@ -1,8 +1,16 @@
 <?php
- $connection = mysqli_connect('localhost', 'root', '', 'loginapp');
-    
-    if(!$connection){
-             
-        die("Database connection failed");
-    }
+
+$db['db_host'] = "localhost";
+$db['db_user'] = 'root';
+$db['db_pass'] = "";
+$db['db_name'] = "cms";
+
+foreach($db as $key => $value){
+    define(strtoupper($key), $value);
+}
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+if($connection){
+    echo "We are connected";
+}
 ?>
